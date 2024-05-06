fa8c5a18fc74792ff0504a81ff75c909900537d7
Soniya1209 <soniyapatidar2016@gmail.com>
2024-05-06 11:07:51 +0530

    state and props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import ClassComponent from './components/ClassComponent';
+import FunctionalComponent from './components/FunctionalComponent';
 
 function App() {
+  const [name, setName] = useState("Soniya");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <h1>Class Component</h1>
+          <ClassComponent></ClassComponent>
+          <h1>Functional Component</h1>
+          <FunctionalComponent  name={name} setName={setName} age={23}/>
     </div>
   );
 }
---
+++ src/components/FunctionalComponent.jsx
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+
+const FunctionalComponent = ({name, age, setName}) => {
+    const [changeName, setChangeName] = useState("");
+    const [count, setCount] = useState(0);
+    return <div>
+        <p>This ia a Functional Component.</p>
+        <button onClick={() => setCount(count + 1)}>Click me to add 1</button>
+        <button onClick={() => setCount(count - 1)}>Click me to subtract 1</button>
+        <h1>{count}</h1>
+        <input onChange={(e) => setChangeName(e.target.value)}/>
+        <button onClick={() => setName(changeName)}>Change name</button>
+        <h1>My name is {name} . I am {age} years old.</h1>
+
+    </div>
+}
+
+export default FunctionalComponent;
