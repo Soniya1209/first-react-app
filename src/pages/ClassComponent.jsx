f64fcd94678b0c0e648032e12305ea9bfa243b79
Soniya1209 <soniyapatidar2016@gmail.com>
2024-05-11 11:09:36 +0530

    React lifecycle methods

--- src/pages/ClassComponent.jsx
+++ src/pages/ClassComponent.jsx
@@ -2,9 +2,35 @@ import React from 'react';
 import BaseHoc from '../hoc/BaseHoc';
 
 class ClassComponent extends React.Component {
+    Name;
+    age;
+    constructor(){
+        super();
+        this.state ={
+            name: "Soniya",
+            age: "23",
+        }
+        console.log("Constructor...");
+    }
+    
+    componentDidMount(){
+        console.log("Component Did Mount");
+    }
+
+    componentDidUpdate(){
+        console.log("Component Did Update");
+        console.log(this.state.age);
+    }
+
     render () {
+        console.log("Render...");
         return <div>
             <p>This is a Class Component.</p>
+            <button onClick={() => this.setState({ 
+                age : this.state.age++,
+                ...this.state}
+            )}>Click me to add 1</button>
+            <h1>{this.state.age}</h1>
         </div>
     }
 }
--- src/pages/FunctionalComponent.jsx
+++ src/pages/FunctionalComponent.jsx
@@ -1,16 +1,34 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import BaseHoc from '../hoc/BaseHoc';
 
 
 const FunctionalComponent = ({name, age, setName}) => {
     const [changeName, setChangeName] = useState("");
     const [count, setCount] = useState(0);
+    const preRef = useRef();
+    
+    useEffect(() => {
+        console.log("Component Did Mount");
+    },[])
+
+    useEffect(() => {
+        console.log("Component Did Update");
+    })
+
+    useEffect(() => {
+        console.log("Only name is updated");
+    },[changeName])
+
+    useEffect(() => {
+        console.log(preRef.current.innerHtML == count);
+    },[count])    
+
     return <div>
 
         <p>This ia a Functional Component.</p>
-        <button onClick={() => setCount(count + 1)}>Click me to add 1</button>
+        <button onClick={() => setCount(count+1)}>Click me to add 1</button>
         <button onClick={() => setCount(count - 1)}>Click me to subtract 1</button>
-        <h1>{count}</h1>
+        <h1 ref={preRef}>{count}</h1>
         <input onChange={(e) => setChangeName(e.target.value)}/>
         <button onClick={() => setName(changeName)}>Change name</button>
         <h1>My name is {name} . I am {age} years old.</h1>
